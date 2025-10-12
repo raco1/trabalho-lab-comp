44eeec812b334fcb9bc6ba0c717b8be5adf8f8f8
rafael <rafaelcoelho2711@gmail.com>
2025-10-12 20:08:41 -0300

    cria app.py e realiza as primeiras configurações

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+login = st.Page(
+    page="pages/login.py",
+    title="Log in",
+    default=True
+)
+
+dashboard = st.Page(
+    page="pages/dashboard.py",
+    title="Dashboard",
+)
+
+sign_up = st.Page(
+    page="pages/signup.py",
+    title="Sign Up",
+)
+
+appointments = st.Page(
+    page="pages/appoitments.py",
+    title="Appointments",
+)
+
+recover_psswd = st.Page(
+    page="pages/recover_psswd.py",
+    title="Recover password",
+)
+
+pg = st.navigation([login, dashboard, sign_up, appointments, recover_psswd])
+
+pg.run()
